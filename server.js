c4886cfc4af7b7c56d15c1690ba10eb0ae38d960
AhmadSElsayed <sama_hamada@hotmail.com>
2018-05-01 17:46:10 +0200

    ESLinter + Music Metadata

--- server.js
+++ server.js
@@ -5,22 +5,21 @@ var fs = require('fs');
 
 
 app.get('/', function (req, res) {
-    var rstream = fs.createReadStream('Music/song.mp3');
-    rstream.pipe(res);
+	var rstream = fs.createReadStream('Music/song.mp3');
+	rstream.pipe(res);
 });
 
 app.get('/data', function (req, res) {
-    var mm = require('musicmetadata');
-    var rstream = fs.createReadStream('Music/song.mp3');
-    var parser = mm(rstream, { duration : true }, function (err, metadata) {
-        if (err) throw err;
-        console.log(metadata);
-    });
-    rstream.pipe(res);
+	var mm = require('musicmetadata');
+	var rstream = fs.createReadStream('Music/song.mp3');
+	mm(rstream, { duration : true }, function (err, metadata) {
+		if (err) throw err;
+		res.write(JSON.stringify(metadata));
+	});
 });
 
 app.set('port', process.env.PORT || 1337);
 
 var server = app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + server.address().port);
+	console.log('Express server listening on port ' + server.address().port);
 });
